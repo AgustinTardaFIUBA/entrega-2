e3d3ae9e78c61945c9dcfb302a152dbd09c5e28c
AgustinTardaFIUBA <atarda@fi.uba.ar>
2020-05-16 19:13:50 -0300

    Operaciones para cuadrados minimo

--- src/entrega-2.py
+++ src/entrega-2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import  xlrd
+import xlrd
 
 book = xlrd.open_workbook('Entrega2.xlsx')
 sheet = book.sheet_by_name('Sheet1')
@@ -14,5 +14,43 @@ for i in range(sheet.nrows):
 plt.plot(cantDias, casos, 'ro')
 plt.axis([0, 180, 0, 8000])
 plt.show()
+
+fises = []
+fi0 = []
+fi1 = []
+fi2 = []
+f = []
+
+for i in range(121):
+    fi0.append(1)
+
+for i in range(121):
+    fi0.append(i)
+
+for i in range(121):
+    iAlCuadrado = i**2
+    fi0.append(iAlCuadrado)
+for i in range(121):
+    casosPorDia = (sheet.cell_value(i, 4))
+    logaritmoDeCasos = np.log(casosPorDia)
+    f.append(logaritmoDeCasos)
+
+fises.append(fi0)
+fises.append(fi1)
+fises.append(fi2)
+
+A = []
+b = []
+for i in range(3):
+    A[i].append([])
+    for j in range(3):
+        A[i][j] = np.dot(fises[i], fises[j])
+
+for i in range(3):
+    result = np.dot(f, fises[i])
+    b.append(result)
+
+print(np.linalg.solve(A, b))
+
 
 
